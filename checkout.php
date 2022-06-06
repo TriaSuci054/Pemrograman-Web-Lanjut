91f18266204d2d5f0e4fabb6a8ca5894c2561f2a
TriaSuci054 <72841608+TriaSuci054@users.noreply.github.com>
2022-06-06 23:36:56 +0700

    Add files via upload

---
+++ checkout.php
@@ -0,0 +1,281 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+include("connection/connect.php");
+include_once 'product-action.php';
+error_reporting(0);
+session_start();
+if(empty($_SESSION["user_id"]))
+{
+	header('location:login.php');
+}
+else{
+
+										  
+												foreach ($_SESSION["cart_item"] as $item)
+												{
+											
+												$item_total += ($item["price"]*$item["quantity"]);
+												
+													if($_POST['submit'])
+													{
+						
+													$SQL="insert into users_orders(u_id,title,quantity,price) values('".$_SESSION["user_id"]."','".$item["title"]."','".$item["quantity"]."','".$item["price"]."')";
+						
+														mysqli_query($db,$SQL);
+														
+														$success = "Thankyou! Your Order Placed Successfully!";
+
+														
+														
+													}
+												}
+?>
+
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="checkout.png">
+    <title>Order Checkout</title>
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/animsition.min.css" rel="stylesheet">
+    <link href="css/animate.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="css/style.css" rel="stylesheet"> </head>
+<body>
+    
+    <div class="site-wrapper">
+        <!--header starts-->
+        <header id="header" class="header-scroll top-header headrom">
+            <!-- .navbar -->
+            <nav class="navbar navbar-dark">
+                <div class="container">
+                    <button class="navbar-toggler hidden-lg-up" type="button" data-toggle="collapse" data-target="#mainNavbarCollapse">&#9776;</button>
+                    <a class="navbar-brand" href="index.php"> <img class="img-rounded" src="images/c.png" alt=""> </a>
+                    <div class="collapse navbar-toggleable-md  float-lg-right" id="mainNavbarCollapse">
+                        <ul class="nav navbar-nav">
+                            <li class="nav-item"> <a class="nav-link active" href="index.php">Home <span class="sr-only">(current)</span></a> </li>
+                            <li class="nav-item"> <a class="nav-link active" href="restaurants.php">Restaurants <span class="sr-only"></span></a> </li>
+                            
+							<?php
+						if(empty($_SESSION["user_id"]))
+							{
+								echo '<li class="nav-item"><a href="login.php" class="nav-link active">Login</a> </li>
+							  <li class="nav-item"><a href="registration.php" class="nav-link active">Sign Up</a> </li>';
+							}
+						else
+							{
+									
+									
+										echo  '<li class="nav-item"><a href="your_orders.php" class="nav-link active">Your Orders</a> </li>';
+									echo  '<li class="nav-item"><a href="logout.php" class="nav-link active">LogOut</a> </li>';
+							}
+
+						?>
+							 
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <!-- /.navbar -->
+        </header>
+        <div class="page-wrapper">
+        <div class="inner-page-hero bg-image" data-image-src="images/img/res.jpeg">
+                <div class="container"> </div>
+                <!-- end:Container -->
+            </div>
+            <div class="result-show">
+                <div class="container">
+                    <div class="row"> 
+                    </div>
+                </div>
+            </div>
+            <div class="top-links">
+                <div class="container">
+                    <ul class="row links">
+                      
+                        <li class="col-xs-12 col-sm-4 link-item"><span>1</span><a href="restaurants.php">Choose Restaurant</a></li>
+                        <li class="col-xs-12 col-sm-4 link-item "><span>2</span><a href="#">Pick your favourite dishes</a></li>
+                        <li class="col-xs-12 col-sm-4 link-item active" ><span>3</span><a href="checkout.php">Get delivered & Pay</a></li>
+                    </ul>
+                </div>
+            </div>
+			
+                <div class="container">
+                 
+					   <span style="color:green;">
+								<?php echo $success; ?>
+										</span>
+					
+                </div>
+            
+			
+			
+				  
+            <div class="container m-t-30">
+			<form action="" method="post">
+                <div class="widget clearfix">
+                    
+                    <div class="widget-body">
+                        <form method="post" action="#">
+                            <div class="row">
+                                
+                                <div class="col-sm-12">
+                                    <div class="cart-totals margin-b-20">
+                                        <div class="cart-totals-title">
+                                            <h4>Cart Summary</h4> </div>
+                                        <div class="cart-totals-fields">
+										
+                                            <table class="table">
+											<tbody>
+                                          
+												 
+											   
+                                                    <tr>
+                                                        <td>Cart Subtotal</td>
+                                                        <td> Rp <?php echo $item_total; ?></td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Shipping &amp; Handling</td>
+                                                        <td>FREE*</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td class="text-color"><strong>Total</strong></td>
+                                                        <td class="text-color"><strong> Rp <?php echo $item_total; ?></strong></td>
+                                                    </tr>
+                                                </tbody>
+												
+												
+												
+												
+                                            </table>
+                                        </div>
+                                    </div>
+                                    <!--cart summary-->
+                                    <div class="payment-option">
+                                        <ul class=" list-unstyled">
+                                            <li>
+                                                <label class="custom-control custom-radio  m-b-20">
+                                                    <input name="mod" id="radioStacked1" checked value="COD" type="radio" class="custom-control-input"> <span class="custom-control-indicator"></span> <span class="custom-control-description">Cash on delivery</span>
+                                                    
+                                            </li>
+                                            <li>
+                                                <label class="custom-control custom-radio  m-b-10">
+                                                    <input name="mod"  type="radio" value="paypal" class="custom-control-input"> <span class="custom-control-indicator"></span> <span class="custom-control-description">Credit Card<img src="images/dicobalah.jpg" alt="" width="90"></span> </label>
+                                            </li>
+                                        </ul>
+                                        <p class="text-xs-center"> <input type="submit" onclick="return confirm('Are you sure?');" name="submit"  class="btn btn-outline-success btn-block" value="Order now"> </p>
+                                    </div>
+									</form>
+                                </div>
+                            </div>
+                       
+                    </div>
+                </div>
+				 </form>
+            </div>
+                    <section class="app-section">
+            
+        </section>
+        <!-- start: FOOTER -->
+        <footer class="footer">
+            <div class="container">
+                <!-- top footer statrs -->
+                <div class="row top-footer">
+                    <div class="col-xs-12 col-sm-3 footer-logo-block color-gray">
+                        <a href="#"> <img src="images/c.png" alt="Footer logo"> </a> <span>Solusi Saat Anda Lapar </span> </div>
+                    <div class="col-xs-12 col-sm-2 about color-gray">
+                        <h5>About Us</h5>
+                        <ul>
+                            <li><a href="#">Social Media</a></li>
+                            <li><a href="#">Testimonials</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-xs-12 col-sm-2 how-it-works-links color-gray">
+                        <h5>Masih Bingung Mau Makan Apa Hari Ini?</h5>
+                        <ul> 
+                            <li><a href="#">Masukkan Lokasi Anda</a></li>
+                            <li><a href="#">Pilih Restaurant</a></li>
+                            <li><a href="#">Pilih Menu Yang Diinginkan</a></li>
+                            <li><a href="#">Silahkan Lakukan Pembayaran</a></li>
+							<li><a href="#">Tunggu Pesanan Anda Dikirm</a></li>
+							<li><a href="#">Selamat Menikmati</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-xs-12 col-sm-2 pages color-gray">
+                        <h5>Legal</h5>
+                        <ul>
+                            <li><a href="#">Terms & Conditions</a> </li>
+                            <li><a href="#">Refund & Cancellation</a> </li>
+                        </ul>
+                    </div>
+                    <div class="col-xs-12 col-sm-3 popular-locations color-gray">
+                        <h5>Lokasi</h5>
+                        <ul>
+                            <li><a href="#">Palembang</a> </li>
+                            <li><a href="#">Solo</a> </li>
+                            <li><a href="#">Surabaya</a> </li>
+                            <li><a href="#">Bali</a> </li>
+                        </ul>
+                    </div>
+                </div>
+                <!-- top footer ends -->
+                <!-- bottom footer statrs -->
+                <div class="bottom-footer">
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-3 payment-options color-gray">
+                        <h5>Cards Payments Available</h5>
+                            <ul>
+                                <li>
+                                    <a href="#"> <img src="images/coba.png" alt="Paypal"> </a>
+                                </li>
+                                <li>
+                                    <a href="#"> <img src="images/bank bri.jpg" alt="Mastercard"> </a>
+                                </li>
+                                <li>
+                                    <a href="#"> <img src="images/dana.jpg" alt="Maestro"> </a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-xs-12 col-sm-4 address color-gray">
+                            <h5>Alamat Utama:</h5>
+                            <p>Surabaya</p>
+                            <h5>Call Center: <a href="tel:+6285745764958">+62 85 745764958</a></h5></div>
+                        <div class="col-xs-12 col-sm-5 additional-info color-gray">
+                            <h5>Who are we?</h5>
+                            <p>Culinary beridiri pada tahun 2020 bertujuan untuk melestarikan makanan khas nusantara</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- bottom footer ends -->
+            </div>
+        </footer>
+        <!-- end:Footer -->
+        </div>
+        <!-- end:page wrapper -->
+         </div>
+
+     <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/tether.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/animsition.min.js"></script>
+    <script src="js/bootstrap-slider.min.js"></script>
+    <script src="js/jquery.isotope.min.js"></script>
+    <script src="js/headroom.js"></script>
+    <script src="js/foodpicky.min.js"></script>
+</body>
+
+</html>
+
+<?php
+}
+?>
